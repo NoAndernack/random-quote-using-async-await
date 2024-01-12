7578487f5ce0c553f5b8d77d596b6fb20897114d
NoAndernack <andernackn@gmail.com>
2024-01-12 14:42:04 +0100

    age

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
 const fetchName = async() => fetch('https://thatsthespir.it/api');
+const fetchName2 = async(name) => fetch(`https://api.agify.io?name=${name}`); // Modifiez la signature de la fonction pour prendre un paramètre 'name'
 const quoteButton = document.querySelector('.quoteButton');
 const quote = document.querySelector('q');
 const img = document.querySelector('.img');
 const author = document.querySelector('.name');
-const loader = document.querySelector("#loader")
+let authorName = "";
+const loader = document.querySelector("#loader");
+const ageAuthor = document.querySelector(".ageAuthor");
+
 document.addEventListener("DOMContentLoaded", () => {
     fetchName()
     .then(response => response.json())
     .then(json => {
+        authorName = json.author;
+            let firstName = authorName.split(' ')[0]; 
+
         quote.textContent = json.quote;
         img.src = json.photo;
-        author.textContent = json.author;
+        author.textContent = authorName;
+        return fetchName2(firstName); 
+    })
+    .then(response => response.json())
+    .then(json => {
+        ageAuthor.textContent = "Age :" + json.age+" ans";
     })
     .catch(error => {
-        alert('Error :', error);
-
+        console.log("There was an error!", error);
     });
-})
+});
 
 quoteButton.addEventListener('click', () => {
-    
     quote.textContent = '';
     img.src = ''; 
     author.textContent = '';
-    
+    ageAuthor.textContent = '';
+
     fetchName()
         .then(response => response.json())
         .then(json => {
+            authorName = json.author;
+            let firstName = authorName.split(' ')[0]; 
+
             quote.textContent = json.quote;
             img.src = json.photo;
-            author.textContent = json.author;
+            author.textContent = authorName;
+
+            return fetchName2(firstName); 
+        })
+        .then(response => response.json())
+        .then(json => {
+            ageAuthor.textContent =  "Age :" + json.age +" ans"; 
         })
         .catch(error => {
-            alert('Error :', error);
-
+            console.log("There was an error!", error);
         });
 });
