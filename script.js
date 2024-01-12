60afef6fcdce1b76b971b203d3f2185f89d64464
NoAndernack <andernackn@gmail.com>
2024-01-12 11:08:02 +0100

    v2

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const quote = document.querySelector('q');
 const img = document.querySelector('.img');
 const author = document.querySelector('.name');
 
+document.addEventListener("DOMContentLoaded", () => {
+    fetchName()
+    .then(response => response.json())
+    .then(json => {
+        quote.textContent = json.quote;
+        img.src = json.photo;
+        author.textContent = json.author;
+    })
+    .catch(error => {
+        alert('Error :', error);
+
+    });
+})
+
 quoteButton.addEventListener('click', () => {
     quote.textContent = '';
     img.src = ''; 
@@ -17,7 +31,7 @@ quoteButton.addEventListener('click', () => {
             author.textContent = json.author;
         })
         .catch(error => {
-            console.error('Error fetching the quote:', error);
+            alert('Error :', error);
 
         });
 });
